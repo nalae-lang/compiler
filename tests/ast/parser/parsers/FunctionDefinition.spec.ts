a24c7183c597760cd2141105a22e2ce6e793f19e
tbvjaos510 <tbvjaos510@naver.com>
2019-12-13 00:57:07 +0900

    chore: github action 추가 (#1)

--- tests/ast/parser/parsers/FunctionDefinition.spec.ts
+++ tests/ast/parser/parsers/FunctionDefinition.spec.ts
@@ -1,4 +1,6 @@
-import { FunctionDefinitionParser } from "ast/parser/parsers/FunctionDefinition";
+import {
+  FunctionDefinitionParser,
+} from "ast/parser/parsers/FunctionDefinition";
 import { ParserTokenTypes } from "ast/types/ParserTokenTypes";
 import snapshot = require("snap-shot-it");
 import { MorphemeTokenTypes } from "token/types/MorphemeTokenTypes";
@@ -9,7 +11,7 @@ import {
   mockIdentifier,
   mockKeyword,
   mockOperator,
-  mockSubject
+  mockSubject,
 } from "../../../helper/lexer/MockToken";
 
 describe("FunctionDifinition", function() {
@@ -20,7 +22,7 @@ describe("FunctionDifinition", function() {
         mockArgument("로"),
         mockIdentifier("이동한다를"),
         mockKeyword("function_define"),
-        mockOperator(",")
+        mockOperator(","),
       ];
       const result = FunctionDefinitionParser(mockTokens, 3);
 
@@ -30,6 +32,29 @@ describe("FunctionDifinition", function() {
       expect(result.subject.subject.name).to.equal("사람");
       expectTokenType(result.arguments[0], MorphemeTokenTypes.ARGUMENT);
       expect(result.arguments[0].names).to.deep.equal(["으로", "로"]);
+
+      snapshot(result);
+    });
+
+    it("콘솔은 ~를 오류로 출력한다를 정의하면,' 일 때", function() {
+      const mockTokens = [
+        mockSubject("콘솔은"),
+        mockArgument("를"),
+        mockIdentifier("오류로"),
+        mockIdentifier("출력한다를"),
+        mockKeyword("function_define"),
+        mockOperator(","),
+      ];
+      const result = FunctionDefinitionParser(mockTokens, 4);
+
+      expectTokenType(result, ParserTokenTypes.FUNCTION_DEFINITION);
+      expect(result.name.name).to.equal("출력한다");
+      expectTokenType(result.subject.subject, MorphemeTokenTypes.IDENTIFIER);
+      expect(result.subject.subject.name).to.equal("콘솔");
+      expectTokenType(result.arguments[0], MorphemeTokenTypes.ARGUMENT);
+      expect(result.arguments[0].names).to.deep.equal(["을", "를"]);
+      expectTokenType(result.arguments[1], MorphemeTokenTypes.IDENTIFIER);
+      expect(result.arguments[1].name).to.equal("오류로");
 
       snapshot(result);
     });
